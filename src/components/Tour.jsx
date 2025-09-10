79f177c2761ab05554eb272f8bbc99b550564d2d
mustah21 <mustafa.ahmad@metropolia.fi>
2025-09-10 14:35:17 +0300

    tour data

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,4 +1,4 @@
-function Tour({ image, date, title, info, location, duration, cost }) {
+function Tour({ image, date, title, info, location, duration, cost, removeTour }) {
   return (
     <article className="tour-card">
       <div className="tour-img-container">
@@ -19,8 +19,11 @@ function Tour({ image, date, title, info, location, duration, cost }) {
           </p>
           <p>from {cost}</p>
           <p>{duration} days</p>
+          <button onClick={removeTour}>Remove</button>
+
         </div>
       </div>
+
     </article>
   );
 }
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,17 +1,34 @@
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
+import { useState } from 'react';
 
 const Tours = () => {
+
+  const [toursData, setToursData] = useState(tours);
+
+  const removeTour = (id) => {
+    setToursData(toursData.filter(tour => tour.id !== id))
+  }
+
+  const restoreTours = () => {
+    setToursData(tours);
+  }
+
+
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+          return <Tour key={tour.id} 
+          {...tour}
+          removeTour = {() => removeTour(tour.id)} />;
         })}
       </div>
+      <button onClick={restoreTours}>Restore</button>
+
     </section>
   );
 };
