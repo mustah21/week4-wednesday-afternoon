253eaf8f669ad8439ccba1495aef1fdc8a94bd43
mustah21 <mustafa.ahmad@metropolia.fi>
2025-09-13 18:02:37 +0300

    updating iteration 5 with useState rather than simple html form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
+import '../App.css'
+import {useState} from 'react';
+
+
 
 const Registration = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const contactUsInformation = {
+            name,
+            email,
+            password
+        }
+        console.log(contactUsInformation); // For debugging
+
+        setName('');
+        setEmail('');
+        setPassword('');
+    }
+
+
+
     return (
         <div>
             <h2>Registration page</h2>
-            <form>
-                <div>
-                    <label htmlFor='name'>Name: </label>
-                    <input id='name' type='text'></input>
-                </div>
-                <div>
-                    <label htmlFor='email'>Email: </label>
-                    <input id='name' type='text'></input>
-                </div>
-                <div>
-                    <label htmlFor='password'>Password: </label>
-                    <input id='password' type='password'></input>
-                </div>
-                <button>Submit</button>
-            </form>
+            <section className='section'>
+                <form onSubmit={onSubmit}>
+                    <div>
+                        <label htmlFor='name'>Name: </label>
+                        <input id='name' type='text' onChange={e => setName(e.target.value)} value={name}></input>
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email: </label>
+                        <input id='name' type='text' onChange={e => setEmail(e.target.value)} value= {email}></input>
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password: </label>
+                        <input id='password' type='password' onChange={e => setPassword(e.target.value)} value={password}></input>
+                    </div>
+                    <button className='btn'>Submit</button>
+                </form>
+
+
+            </section>
         </div>
     )
 }
 
-export default Registration
+export default Registration;
